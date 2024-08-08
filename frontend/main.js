391a50b4ac477bfa7d7f049e6d4a35b683974768
Mariam Bello <sheactuary@gmail.com>
2024-08-08 13:50:22 +0100

    updated html structure and content, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log ("Websire called function Api");
+        count =response.count;
+        document.getElementById("counter").innerText =count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
